d49256abe2689b6702c8e624f37f2eed6c49a14f
Mattia <mattia.ducci@intersail.it>
2021-11-09 15:48:41 +0100

    creata pagina prodotti

---
+++ pages/products.js
@@ -0,0 +1,16 @@
+const Products = () => {
+    return (
+        <>
+        <h1>Cool product</h1>
+        <p>This is a very cool product</p>
+        <p>2.50€</p>
+        <hr/>
+        <h1>Cool product 2</h1>
+        <p>This is an even cooler product</p>
+        <p>20.50€</p>
+        <hr/>
+        </>
+    )
+};
+
+export default Products;
